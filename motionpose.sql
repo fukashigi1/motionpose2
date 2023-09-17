a2f8374f81a26b54e9cef2f4b36b51d3457109c1
fukashigi1 <82066248+fukashigi1@users.noreply.github.com>
2023-09-17 00:28:31 -0300

    17.09.2023.0028 nueva db

--- motionpose.sql
+++ motionpose.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 05-09-2023 a las 23:13:57
+-- Tiempo de generación: 17-09-2023 a las 03:27:43
 -- Versión del servidor: 8.0.30
 -- Versión de PHP: 8.1.10
 
@@ -28,22 +28,36 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `usuarios` (
-  `id` int NOT NULL,
-  `nombre_usuario` varchar(100) COLLATE utf8mb4_spanish2_ci NOT NULL,
-  `contrasena` varchar(100) COLLATE utf8mb4_spanish2_ci NOT NULL,
-  `correo` varchar(100) COLLATE utf8mb4_spanish2_ci NOT NULL,
-  `tipo_usuario` int NOT NULL
+  `nombre_usuario` varchar(150) COLLATE utf8mb4_spanish2_ci NOT NULL,
+  `contrasena` varchar(150) COLLATE utf8mb4_spanish2_ci NOT NULL,
+  `correo` varchar(150) COLLATE utf8mb4_spanish2_ci NOT NULL,
+  `tipo_usuario` int NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `nombre_usuario`, `contrasena`, `correo`, `tipo_usuario`) VALUES
-(1, 'test1', 'contraseñatest', 'test1@gmail.com', 0),
-(2, 'Mathias', 'papa', 'correo', 0),
-(3, 'VIP', 'contrasena', 'correo@gmail.com', 1),
-(4, 'ADMIN', 'contrasena', 'correo@gmail.com', 2);
+INSERT INTO `usuarios` (`nombre_usuario`, `contrasena`, `correo`, `tipo_usuario`) VALUES
+('123', '$2b$12$AapRS5dYRx9f.0R0Re7Lre2UTzOiN92MJk3QU56oyx6beyDN7Azh2', '123@gmail.com', 0),
+('123', '$2b$12$qXbkZu/Mn6ZhYjBztTWszuOqVLdtUIPr/aqp2kzyr7rCf/nzQA2PC', '1234@gmail.com', 0),
+('hola', '$2b$12$LKfI3kGwShNQaRzc0lKYEOmf88vGjLbMKPaipIW0aFBim.ZL2k5Uy', 'maatiaasbkn2@gmail.com', 0),
+('hola', '$2b$12$.qUieb/BbTCntFohJHJYmO0jPsRJHqE9BKEEH5xw5dtpnrjg6phhu', 'maatiaasbkn3@gmail.com', 0),
+('aaaa', '$2b$12$4TKtwrkI33HQjqjHSppvqukxQe4DNLIxrqqWs5Qa.ykTacuqzgDNC', 'maatiaasbkn4@gmail.com', 0),
+('sexador', '$2b$12$dYq34QowNT6nexQq3fL8Mu9LWBptNJSpRDjnLJuxbJchUQs5wTLUG', 'sexador@gmail.com', 0),
+('test1', '$2b$12$muphf2Xu/TYv5SU4a0obPOgKh/YsIgRWqp0Fdt1zqTufQ7X6cu6Bm', 'test1@gmail.com', 0),
+('test10', '$2b$12$nncl9WMTyBe7xWi2.Wyq0u0rgP9Lt9Riq5ZUQ5KyivhUcx7/6d/w6', 'test10@gmail.com', 0),
+('test11', '$2b$12$DnEBYHKwCGU2He4pwwOfyuP8qVG/AEwCjsKwH5NXcq45t50SQC8I2', 'test11@gmail.com', 0),
+('test12', '$2b$12$2Wei9NgPBgAbDolVvjy.Xu6hXQZOYGDmho.JBsBounfGUzq7yOIfC', 'test12@gmail.com', 0),
+('test13', '$2b$12$qvgIIX1/A5xIgyfi7Lybdejmu.qHghQZkq6ycDLTCBvIXP29Xcx9W', 'test13@gmail.com', 0),
+('test2', '$2b$12$VDaTZ0tRmzImIyF0Sy6ty.io6OgbcjFIty2RN346y8ifRW4uvLAr2', 'test2@gmail.com', 0),
+('test3', '$2b$12$LT5nJdCZzD7K2FILPcovDOhYWCNJpO3D19oFQJTsbVb05KHgjg22G', 'test3@gmail.com', 0),
+('test4', '$2b$12$6IWtEMBJcfN70amDja6YjOxyQP/dAu4220uXbVN3PA1/BY0bdLqOO', 'test4@gmail.com', 0),
+('test5', '$2b$12$Qad9q7ClA1pZVsFot7TcMOjTTdQZDhKOrdG6usaXWWs5HybQMfrde', 'test5@gmail.com', 0),
+('test6', '$2b$12$IYWnhlr2sf3g2OFHPKnqqeW/8iupuGwufZBjF4.4lYJMYvA/SXm6a', 'test6@gmail.com', 0),
+('test7', '$2b$12$xuFxNx5HHKqUXkGhgv8vzuwN3u.Or5GlCnA8xkL8jWuFhbt/.I9nG', 'test7@gmail.com', 0),
+('test8', '$2b$12$7UEOj9Fi0vfiiq1E8UTvveeq8BGhVoy9QaW6QBXvs6LLCYrp9LxJC', 'test8@gmail.com', 0),
+('test9', '$2b$12$I9wG0jtfTy0gxXCVcahEuOqvRSM5F/VJ65NV/XkFeejYKQv8ndpQm', 'test9@gmail.com', 0);
 
 --
 -- Índices para tablas volcadas
@@ -53,19 +67,10 @@ INSERT INTO `usuarios` (`id`, `nombre_usuario`, `contrasena`, `correo`, `tipo_us
 -- Indices de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  ADD PRIMARY KEY (`correo`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
