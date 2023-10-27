8129b457d9eea5aa387b89f4f961936027fa2d8a
Camilo Ignacio Pereira Lainati <camilo.pereira@impact.cl>
2023-10-27 14:53:26 -0300

    Implementación de varias mejoras, diseño responsivo y demás

--- public/js/footer.js
+++ public/js/footer.js
@@ -1,50 +1,41 @@
 $(document).ready(function(){
+    if ($(window).width() <= 1080 && $(window).height() <= 2600) {
+        $(".navMenu").hide();
+        $(".navMenu").slideUp();
+    }
     let footer = '';
     footer += '<footer>';
     footer += '    <p>Motion Pose MP ~ 2023</p>';
-    footer += '    <a id="politicas">Políticas de privacidad</a>';
+    footer += '    <div class="footerButtons"><a id="politicas">Políticas de privacidad</a>';
     footer += '    <a id="terminos">Términos y condiciones</a>';
-    footer += '    <a id="manual">Manual de usuario</a>';
+    footer += '    <a id="manual">Manual de usuario</a></div>';
     footer += '    <a id="volverArriba" style="float: right;">Volver arriba</a>';
     footer += '</footer>';
     
     $('body').append(footer);
 
-    $('footer').css({
-        'margin-left': '11vw',
-        'margin-right': '11vw',
-        'margin-top': '26vh',
-        'border-top': '1px solid white',
-        'margin-bottom': '7vh'
-    });
-
-    $('footer p').css({
-        'font-size': '1.47vw',
-        'margin-left': '1.85vw'
-    });
-
-    $('footer a').css({
-        'font-size': '1.45vw',
-        'margin-left': '1.80vw',
-        'cursor': 'pointer'
-    });
-
-    $('footer a').hover(function() {
-        $(this).css('text-decoration', 'underline');
-    }, function() {
-        $(this).css('text-decoration', 'none');
-    });
-
     $(window).scroll(function() {
         var distanciaTop = $(window).scrollTop();
         if (distanciaTop >= 20) {
             $(".header").css("background-color", "#16161a");
-        } else {
+          } else {
             $(".header").css("background-color", "#ffffff00");
+          }
+    });
+
+    $('.dropdown-menu').on('click', function(){
+        $(".navMenu").slideToggle(300);
+        $(".header").toggleClass('header-top');
+    });
+
+    $(document).on('click', function(event) {
+        if (!$(event.target).closest('.dropdown-menu').length) {
+            $(".navMenu").slideUp(300);
+            $(".header").removeClass('header-top');
         }
     });
 
-    $('#volverArriba').on('click', function(e) {
+    $('.imgLogo, #volverArriba').on('click', function(e) {
         e.preventDefault();
         $('html, body').animate({scrollTop:0}, '300');
     });
