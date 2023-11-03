557fe18c844aa0e3db517d4b2735730338049a00
fukashigi1 <82066248+fukashigi1@users.noreply.github.com>
2023-11-03 16:29:01 -0300

    0.7.1.03112023

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    v = '0.5.3.17102023';
+    v = '0.7.1.03112023';
     let version = '';
     version = '<div style="position: sticky; font-size: 2vh; color: white; bottom: 0;">' + v + '</div>';
 
